7f49d540c5fbce9b0619a8b1d88073d676dbc216
Murilo <murilobauck2@gmail.com>
2025-03-24 17:00:27 -0300

    parametros fs

--- app.js
+++ app.js
@@ -1,14 +1,36 @@
-const http = require('http');
+import http from 'http';
+import fs from 'fs';
 
-const servidor = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain; charset=utf-8');
-  res.end('Hello World')
+fs.writeFile('./mensagem.txt', 'Hello World!', 'utf-8', (erro) => {
+  if (erro){
+    console.log('Falha ao esrevre o arquivo', erro);
+    return;
+  }
+  console.log('Arquivo foi criado com sucesso');
 });
 
-const porta = 3000;
-const host = 'localhost';
+fs.readFile('./mensagem.txt', 'utf-8', (erro, conteudo) => {
+  if (erro){
+    console.log('Falha ao ler o arquivo', erro);
+    return;
+  }
 
-servidor.listen(porta, host, () => {
-  console.log(`Servidor rodando em http://${host}:${porta}/`); 
+  console.log(`Conteúdo do arquivo: ${conteudo}`);
+  
+  iniciarServidorHttp(conteudo);
 });
+
+function iniciarServidorHttp(mensagem){
+  const servidor = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain; charset=utf-8');
+    res.end(mensagem)
+  });
+  
+  const porta = 3000;
+  const host = 'localhost';
+  
+  servidor.listen(porta, host, () => {
+    console.log(`Servidor rodando em http://${host}:${porta}/`);
+  });
+}
